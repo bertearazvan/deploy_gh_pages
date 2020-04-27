2d91581da7b68cc0d0eca91e085316b11c76e01e
bertearazvan <bertea74@gmail.com>
2020-04-27 10:20:29 +0200

    auto-commit

---
+++ react_app/precache-manifest.14bb2f9d7c37d957a5021963856a4409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5eef6c2a2fcdd13aa267d8967359ed",
+    "url": "/deploy_gh_pages/react_app/index.html"
+  },
+  {
+    "revision": "bbce08f8d93eb08e3f69",
+    "url": "/deploy_gh_pages/react_app/static/css/main.62cfe094.chunk.css"
+  },
+  {
+    "revision": "0ac77afe14a1566330d4",
+    "url": "/deploy_gh_pages/react_app/static/js/2.74244aa3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/deploy_gh_pages/react_app/static/js/2.74244aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbce08f8d93eb08e3f69",
+    "url": "/deploy_gh_pages/react_app/static/js/main.da86fedc.chunk.js"
+  },
+  {
+    "revision": "377c0e5de05d72cb099c",
+    "url": "/deploy_gh_pages/react_app/static/js/runtime-main.7207db1d.js"
+  }
+]);
